4ee06a47536327da67e477c3bcdaa0aa78fcb470
Shailesh Vasandani <github@shaile.sh>
2020-08-23 09:59:12 -0400

    Add sentinel and multiple locks for better concurrency

--- main.cpp
+++ main.cpp
@@ -5,8 +5,11 @@ using namespace std;
 #include <thread>
 #include <mutex>
 #include <chrono>
+#include <cstdlib>
 
-mutex mtx;
+mutex mhead;
+mutex mtail;
+mutex msent;
 
 class Node {
     string data;
@@ -31,33 +34,33 @@ class Node {
     }
 };
 
-void enqueue(Node **head, Node **tail, string data) {
+void enqueue(Node *sentinel, Node **tail, string data) {
     Node *n = new Node(data);
     
-    mtx.lock();
+    mtail.lock();
     if (*tail == NULL) {
-        *head = n;
+        sentinel->setNext(n);
         *tail = n;
     } else {
         (*tail)->setNext(n);
 
         *tail = n;
     }
-    mtx.unlock();
+    mtail.unlock();
 }
 
-Node* dequeue(Node **head, Node **tail) {
-    mtx.lock();
+Node* dequeue(Node *sentinel, Node **head) {
+    mhead.lock();
+    msent.lock();
 
     Node *toReturn = *head;
 
     *head = (*head)->getNext();
 
-    if (*head == NULL) {
-        *tail = NULL;
-    }
+    sentinel->setNext(*head);
 
-    mtx.unlock();
+    msent.unlock();
+    mhead.unlock();
 
     toReturn->setNext(NULL);
 
@@ -72,29 +75,46 @@ void print(Node *head) {
     }
 }
 
-void doDequeue(Node **head, Node **tail) {
+void doDequeue(Node *sentinel, Node **head) {
     // dequeue(head, tail);
-    cout << dequeue(head, tail)->getData() << endl;
+    cout << dequeue(sentinel, head)->getData() << endl;
 }
 
 int main() {
     Node *head = new Node("head node");
     Node *tail = head;
 
-    enqueue(&head, &tail, "next 1");
-    enqueue(&head, &tail, "next 2");
-    enqueue(&head, &tail, "next 3");
-    enqueue(&head, &tail, "next 4");
-    enqueue(&head, &tail, "next 5");
+    Node *sentinel = new Node("");
+    sentinel->setNext(head);
+
+    enqueue(sentinel, &tail, "next 1");
+    enqueue(sentinel, &tail, "next 2");
+    enqueue(sentinel, &tail, "next 3");
+    enqueue(sentinel, &tail, "next 4");
+    enqueue(sentinel, &tail, "next 5");
 
     print(head);
     
-    thread dequeueThreads[5];
+    thread threads[1000];
 
-    for (int i=0; i<5; ++i)
-        dequeueThreads[i] = thread(doDequeue, &head, &tail);
+    for (int i=0; i<500; ++i) {
+        if (rand() % 3 == 0) {
+            threads[i] = thread(doDequeue, sentinel, &head);
+        } else {
+            threads[i] = thread(enqueue, sentinel, &tail, "next " + to_string(i));
+        } 
+    }
 
-    for (auto& th : dequeueThreads) th.join();
+    for (int i=500; i<1000; ++i) {
+        if (rand() % 3 == 0) {
+            threads[i] = thread(enqueue, sentinel, &tail, "next " + to_string(i));
+        } else {
+            threads[i] = thread(doDequeue, sentinel, &head);
+        } 
+    }
+        
+
+    for (auto& th : threads) th.join();
 
     // print(head);
 
